8a9ca6d42fe48b6d60f6b79d2d6460a95c5bd147
Dexter <dexter.t.jones@outlook.com>
2024-01-17 21:40:53 -0700

    Pseudocode and variables added

---
+++ Temperatures/src/Main.java
@@ -0,0 +1,36 @@
+/*
+Written by Dexter Jones
+Option #2 Get Monthly Temperatures
+
+Pseudocode:
+Program that uses two arrays to store monthly temperatures (one stores months, one stores temperatures)
+Program prompts user for the month to be viewed - outputs and average temp for respective month
+If "year" entered, output is all months with respective temps, the yearly average, and highest & lowest monthly averages
+
+input:
+String: month/year
+
+output:
+String: month/s
+float: monthly average, high average, low average
+
+arrays: String months, float temp
+
+user validation
+ */
+public class Main {
+    public static void main(String[] args) {
+        String month;
+        float temp = 0, high, low;
+        String months[] = new String[12];
+        float temps[] = new float[12];
+        boolean isFormat = false;
+
+        System.out.println("Monthly Average Temperature Index");
+
+        for (int i = 0; i < 12; i++) {
+
+        }
+
+    }
+}
